316fab344fa118e6a55525e0dc044779c424a097
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-09-24 17:49:18 +0500

    backend

--- src/components/AdminProtected.jsx
+++ src/components/AdminProtected.jsx
@@ -3,9 +3,27 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminProtected = () => {
-    const user = useSelector((state) => state.auth);
+    // Check both auth and adminInfo from Redux
+    const auth = useSelector((state) => state.auth);
+    const adminInfo = useSelector((state) => state.orebiReducer?.adminInfo);
+    const userInfo = useSelector((state) => state.orebiReducer?.userInfo);
+    
+    // Check if user is logged in (either as admin or regular user)
+    const isLoggedIn = auth?.id !== "" || adminInfo?.id || userInfo?.id;
+    
+    // For development: Allow access if any user is logged in
+    // For production: You can add role check here
+    const hasAccess = isLoggedIn;
+    
+    console.log('AdminProtected check:', {
+        auth,
+        adminInfo,
+        userInfo,
+        isLoggedIn,
+        hasAccess
+    });
 
-    return user.id !== "" ? <Outlet /> : <Navigate to="/login" replace />;
+    return hasAccess ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default AdminProtected;
